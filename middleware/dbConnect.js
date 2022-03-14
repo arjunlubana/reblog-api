4bb6baa0fa55e76e5f1282968347a4c9a1e0bc0c
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-14 14:36:47 +0300

    Use SSL for connections

--- middleware/dbConnect.js
+++ middleware/dbConnect.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require("sequelize");
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 function connectToDatabase() {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
   sequelize.sync({ alter: true });
   return sequelize;
