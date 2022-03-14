c6b6d198a7adf9929f5ff6b041178e031511dcb1
Arjun Lubana <arjunlubana562@gmail.com>
2022-03-14 13:51:05 +0300

    Use dotenv only in development

--- middleware/dbConnect.js
+++ middleware/dbConnect.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 function connectToDatabase() {
   var sequelize = new Sequelize(process.env.DATABASE_URL);
