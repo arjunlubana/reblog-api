bb4f1143e18355b4e8ea575606dc303108ec1182
Arjun Lubana <arjunlubana562@gmail.com>
2022-04-30 15:39:07 +0300

    BugFix: Change the issuerBaseURL.

--- src/middleware/auth0.middleware.js
+++ src/middleware/auth0.middleware.js
@@ -2,7 +2,7 @@ const { auth } = require("express-oauth2-jwt-bearer");
 
 const checkJwt = auth({
 	audience: "https://reblog-api.herokuapp.com",
-	issuerBaseURL: "https://reblog-api.herokuapp.com",
+	issuerBaseURL: "https://reblog.us.auth0.com/",
 });
 
 module.exports = checkJwt;
