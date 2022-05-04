e58006b1c7246ecb7b3ecd373585eb05b5f21114
Arjun Lubana <arjunlubana562@gmail.com>
2022-05-04 07:48:10 +0300

    Add Auth0 config from environment variables

---
+++ src/config/auth0.config.js
@@ -0,0 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require("dotenv");
+  dotenv.config();
+}
+
+module.exports = {
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+};
