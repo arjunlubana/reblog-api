663f3ac0e34c0e01196a403968fddda630005c0b
Arjun Lubana <arjunlubana562@gmail.com>
2022-05-04 15:56:21 +0300

    Increase the timeout duration for verifying tokens

--- src/middleware/auth0.middleware.js
+++ src/middleware/auth0.middleware.js
@@ -3,6 +3,7 @@ const { auth } = require("express-oauth2-jwt-bearer");
 const checkJwt = auth({
 	audience: "https://reblog-api.herokuapp.com",
 	issuerBaseURL: "https://reblog.us.auth0.com/",
+	timeoutDuration: 30000
 });
 
 module.exports = checkJwt;
