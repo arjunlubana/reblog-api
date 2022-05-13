ac1bb242caad283e6724d5cfbf78e21732c831ef
Arjun Lubana <arjunlubana562@gmail.com>
2022-05-13 05:39:53 +0300

    Delete unneeded fetchUser service

--- src/controllers/blogs/postBlog.js
+++ src/controllers/blogs/postBlog.js
@@ -1,10 +1,9 @@
 const { createBlog } = require("../../services/blogs.service");
-const { fetchUser } = require("../../services/auth0.service");
 const ServerError = require("../../errors/serverError");
 
 async function postBlog(req, res, next) {
   try {
-    req.body.author = req.auth.payload.sub;
+    // req.body.author = req.auth.payload.sub;
     const blog = await createBlog(req.body);
     res.send(blog);
   } catch (err) {
