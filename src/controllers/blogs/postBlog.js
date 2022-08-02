cc74b4d8b29b87071d49832806700f9a45aff9e0
Arjun Lubana <arjunlubana562@gmail.com>
2022-08-02 06:09:44 +0300

    Fetch blog post purely

--- src/controllers/blogs/postBlog.js
+++ src/controllers/blogs/postBlog.js
@@ -1,16 +1,10 @@
 const { createBlog } = require('../../services/blogs')
-const auth0Manager = require('../../services/auth0Manager')
+
 async function postBlog(req, res, next) {
   try {
     req.body.authorId = req.auth.sub
     const blog = await createBlog(req.body)
-    await auth0Manager.getUser(
-      { id: "auth0|6270c3055b7a82006f921ec8" },
-      function (err, user) {
-        blog.authorId = user.name
-        res.json(blog)
-      }
-    )
+    res.json(blog)
   } catch (error) {
     next(error)
   }
--- src/services/blogs/fetchBlog.js
+++ src/services/blogs/fetchBlog.js
@@ -1,10 +1,8 @@
-const { Blog, User } = require('../../db/models')
+const { Blog } = require('../../db/models')
 const { ClientError } = require('../../errors')
 
 async function fetchBlog(id) {
-  const blog = await Blog.findByPk(id, {
-    include: User
-  })
+  const blog = await Blog.findByPk(id)
   if (!blog) {
     throw new ClientError(
       'Resource Not Found',
